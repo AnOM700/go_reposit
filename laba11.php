cb4ec58fd068b706c1da2a1b3bc3c93c503d96d7
AnOM700 <example@example.com>
2026-03-25 20:25:05 +0300

    2.Задание

--- laba11.php
+++ laba11.php
@@ -2,6 +2,12 @@
     $file=fopen("test.txt","w");
     fwrite($file,"Привет мир");
     fclose($file);
+$file =fopen("test.txt","r");
+    while (!feof($file))
+{
+$str = fgets($file);
+echo $str."<br />";
+}
 
 
 ?>
