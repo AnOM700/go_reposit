a782b3dd089c103d1cc067243c3a03e06df27968
AnOM700 <example@example.com>
2026-03-25 20:29:55 +0300

    6.Задание

--- laba11.php
+++ laba11.php
@@ -12,6 +12,7 @@ rename("test.txt","mir.txt");
 mkdir("folder",0700);
 rename("mir.txt","folder/mir.txt");
 copy("folder/mir.txt","folder/world.txt");
-
+$size=filesize("folder/world.txt");
+echo "В байтах: ".$size." В мегабайтах: ".($size/1024)." В гигабайтах: ".($size/(1024*1024));
 
 ?>
