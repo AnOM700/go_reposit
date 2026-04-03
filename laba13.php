484cfe890744bd5f7a1cabdb9e762116cc1f9d96
AnOM700 <example@example.com>
2026-04-03 12:05:19 +0300

    Третье, Четвертое, Пятое Задание

--- laba13.php
+++ laba13.php
@@ -1,20 +1,36 @@
 <?php
 
 
-  //ПУНКТ 1: Создаем класс Worker
+  //ПУНКТ 1
  
 class Worker {
-    // Свойства (Пункт 1)
+    // ПУНКТ 1
     public $name;
     public $age;    
     public $salary;
+// Задания 3,4,5
+public function getName() {
+    return $this->name;
+}
+
+public function getAge() {
+    return $this->age;
+}
+
+public function getSalary() {
+    return $this->salary;
+}
+
+
+
+
 }
 
 // --- ВЫПОЛНЕНИЕ ПО ПУНКТАМ ---
 
 echo "<h3>Результаты выполнения задания:</h3>";
 
-// ПУНКТ 1: Создаем 2 объекта и устанавливаем значения
+// ПУНКТ 1
 $worker1 = new Worker();
 $worker1->name = "Иван";
 $worker1->salary = 1000;
@@ -24,11 +40,14 @@ $worker2 = new Worker();
 $worker2->name = "Василий";
 $worker2->salary = 2000;
 $worker2->age(30);
-// ПУНКТ 2: Выводим сумму зарплат и возрастов
+// ПУНКТ 2
 echo "Сумма зарплат: " . ($worker1->salary + $worker2->salary) . "<br>";
 echo "Сумма возрастов: " . ($worker1->age + $worker2->age) . "<br>";
 
-
+// ПУНКТ 3,4,5
+echo "Имя: " . $worker1->getName() . "<br>";
+echo "Возраст: " . $worker1->getAge() . "<br>";
+echo "Зарплата: " . $worker1->getSalary() . "<br>";
 
 
 ?>
