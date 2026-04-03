4fc968dff844c38c2f87364a08a6099790477468
AnOM700 <example@example.com>
2026-04-03 11:40:29 +0300

    Второе задание

--- laba13.php
+++ laba13.php
@@ -1,8 +1,8 @@
 <?php
 
-/**
- * ПУНКТ 1: Создаем класс Worker
- */
+
+  //ПУНКТ 1: Создаем класс Worker
+ 
 class Worker {
     // Свойства (Пункт 1)
     public $name;
@@ -18,12 +18,16 @@ echo "<h3>Результаты выполнения задания:</h3>";
 $worker1 = new Worker();
 $worker1->name = "Иван";
 $worker1->salary = 1000;
-$worker1->setAge(25); 
+$worker1->age(25); 
 
 $worker2 = new Worker();
 $worker2->name = "Василий";
 $worker2->salary = 2000;
-$worker2->setAge(30);
+$worker2->age(30);
+// ПУНКТ 2: Выводим сумму зарплат и возрастов
+echo "Сумма зарплат: " . ($worker1->salary + $worker2->salary) . "<br>";
+echo "Сумма возрастов: " . ($worker1->age + $worker2->age) . "<br>";
+
 
 
 
