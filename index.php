334cf6056034f7e8d70db1f84a2abad6c38c0492
AnOM700 <your@example.com>
2026-04-09 15:51:27 +0300

    3.Task

--- index.php
+++ index.php
@@ -18,5 +18,33 @@ class Page {
 
 }
 
+class BlogPage extends Page{
+    public function __construct(){
+
+        $this->name = "blog";
+        $this->template="
+        <div style='display: flex; gap: 20px;'>
+            <h1>Что появилось раньше: яйцо или курица?</h1>
+                <div style='border: 1px solid #000; padding:10px;'>
+                    <img src='Images/chicken.jpg' alt='img'/>
+                    <h3>Курица</h3>
+                    <p>Подробнее о курице...</p>
+                </div>
+                <div style='border: 1px solid #000; padding:10px;'>
+                    <img src='Images/egg.jpg'/>
+                    <h3>Яйцо</h3>
+                    <p>Подробнее о яйце</p>
+                </div>
+        </div>"
+
+
+
+    }
+
+
+
+
+}
+
 
 ?>
