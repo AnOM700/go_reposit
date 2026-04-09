7f6de63b2f43265e508fd6441b0649ee93001897
AnOM700 <example@example.com>
2026-04-09 20:52:04 +0300

    5,6. Tasks

--- laba14.php
+++ laba14.php
@@ -1,12 +1,12 @@
 <?php
 class Page {
-    private string $name;
-    private string $template;
+    protected string $name;
+    protected string $template;
 
     public function __construct(){
     
         $this->name="page";
-        $this->template="<div><p>It is a default page</p></div>"        
+        $this->template="<div><p>It is a default page</p></div>";        
         
     }
 
@@ -23,19 +23,22 @@ class BlogPage extends Page{
 
         $this->name = "blog";
         $this->template="
-        <div style='display: flex; gap: 20px;'>
+        <div>
             <h1>Что появилось раньше: яйцо или курица?</h1>
+            <div style='display: flex;  gap: 20px; align-items:flex-start;'>
                 <div style='border: 1px solid #000; padding:10px;'>
-                    <img src='Images/chicken.jpg' alt='img'/>
+                    <img src='Images/chicken.jpg' alt='img' width=250px/>
                     <h3>Курица</h3>
                     <p>Подробнее о курице...</p>
-                </div>
-                <div style='border: 1px solid #000; padding:10px;'>
-                    <img src='Images/egg.jpg'/>
+                </div><br>
+                <div style='border: 1px solid #000; padding:10px; height:285px '>
+                    <img src='Images/egg.jpg' width=250px/>
                     <h3>Яйцо</h3>
-                    <p>Подробнее о яйце</p>
+                    <p>Подробнее о яйце...</p>
                 </div>
-        </div>"
+
+            </div>
+        </div>";
 
 
 
@@ -44,6 +47,31 @@ class BlogPage extends Page{
 
 
 
+}
+class HomePage extends Page {
+    public function __construct() {
+        $this->name = "home";
+        $this->template = "
+            <div style='background: #e1f5fe; padding: 20px; border-radius: 5px;'>
+                <h1>Добро пожаловать на Главную!</h1>
+                <p>Это первая страница в соответствии с заданием.</p>
+            </div>";
+    }
+}
+
+$currentPage = $_GET['page'] ?? 'home'; 
+
+switch ($currentPage) {
+    case 'blog':
+        $view = new BlogPage();
+        break;
+    case 'page':
+        $view = new Page();
+        break;
+    case 'home':
+    default:
+        $view = new HomePage();
+        break;
 }
 ?>
 
@@ -65,9 +93,14 @@ class BlogPage extends Page{
 <a href="?page=page">2. Стандартная страница</a>
 <a href="?page=blog">3. Блог(Карточки)</a>
     </nav>
+    <div>
+        <?php
+
+            $view->render();
+        ?>
+    </div>
 </body>
 </html>
 
 
-?>
 
