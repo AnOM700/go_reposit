7e9d1c5095eeb5da2c693741d249d3aaefc460f2
AnOM700 <example@example.com>
2026-04-10 18:26:24 +0300

    Создан полноценный класс Прямоугольник

--- laba15.php
+++ laba15.php
@@ -11,4 +11,25 @@ abstract class Figure {
 
     abstract public function infoAbout();
 }
+class Rectangle extends Figure implements CalculatableArea {
+    private $a;
+    private $b;
+
+    public function __construct($a, $b, $color = "black") {
+        $this->a = $a;
+        $this->b = $b;
+        $this->color = $color;
+        $this->sidesCount = 4;
+    }
+
+    public function getArea() {
+        $this->area = $this->a * $this->b;
+        return $this->area;
+    }
+
+    public function infoAbout() {
+        return "Это класс прямоугольника. У него {$this->sidesCount} стороны.";
+    }
+}
+
 ?>
