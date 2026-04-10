329da6c841a38c347befafe0b96ba80c8e909921
AnOM700 <example@example.com>
2026-04-10 18:29:53 +0300

    Создан полноценный класс Треугольник

--- laba15.php
+++ laba15.php
@@ -50,6 +50,29 @@ class Square extends Figure implements CalculatableArea {
         return "Это класс квадрата. У него {$this->sidesCount} стороны.";
     }
 }
+class Triangle extends Figure implements CalculatableArea {
+    private $a;
+    private $b;
+    private $c;
+
+    public function __construct($a, $b, $c, $color = "green") {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+        $this->color = $color;
+        $this->sidesCount = 3;
+    }
+
+    public function getArea() {
+        $p = ($this->a + $this->b + $this->c) / 2;
+        $this->area = sqrt($p * ($p - $this->a) * ($p - $this->b) * ($p - $this->c));
+        return round($this->area, 2);
+    }
+
+    public function infoAbout() {
+        return "Это класс треугольника. У него {$this->sidesCount} стороны.";
+    }
+}
 
 
 ?>
