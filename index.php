8ae6ea0a9268d118c3ab981579c63a777b072d41
AnOM700 <your@example.com>
2026-04-27 13:55:57 +0300

    Создана структура проекта

--- index.php
+++ index.php
@@ -1,50 +1,16 @@
-<?php
-class Page {
-    private string $name;
-    private string $template;
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <?php include 'includes/header.php'; ?>
+    <main>
+        <h1>Добро пожаловать!</h1>
+    </main>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
 
-    public function __construct(){
-    
-        $this->name="page";
-        $this->template="<div><p>It is a default page</p></div>"        
-        
-    }
-
-    public function render(){
-        echo $this->template;
-    
-    }
-
-
-}
-
-class BlogPage extends Page{
-    public function __construct(){
-
-        $this->name = "blog";
-        $this->template="
-        <div style='display: flex; gap: 20px;'>
-            <h1>Что появилось раньше: яйцо или курица?</h1>
-                <div style='border: 1px solid #000; padding:10px;'>
-                    <img src='Images/chicken.jpg' alt='img'/>
-                    <h3>Курица</h3>
-                    <p>Подробнее о курице...</p>
-                </div>
-                <div style='border: 1px solid #000; padding:10px;'>
-                    <img src='Images/egg.jpg'/>
-                    <h3>Яйцо</h3>
-                    <p>Подробнее о яйце</p>
-                </div>
-        </div>"
-
-
-
-    }
-
-
-
-
-}
-
-
-?>
